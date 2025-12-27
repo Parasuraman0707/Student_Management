234e4485f9c74ae0acbf28f3acb87d583c69a315
Parasuraman0707 <parasuramangandhi327@gmail.com>
2025-12-27 14:13:52 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,9 @@ import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-         host="srv1834.hstgr.io",
+        host="srv1834.hstgr.io",
         user="u651328475_batch_11",
         password="Batch_11",
         database="u651328475_batch_11",
     )    
+
